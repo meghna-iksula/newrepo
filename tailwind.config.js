43c00c01318338d82070f85df4ffff1896d4db45
meghnaerla <meghna.e@iksula.com>
2023-04-17 16:56:08 +0530

    Added custom colors and fonts.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: {
+          850: "#9a1a28",
+        },
+        moss: {
+          default: "#a9b089",
+        },
+        zinc: {
+          250: "#e0e1dd",
+          350: "#697c6b",
+          750: "#252c26",
+        },
+      },
+      fontFamily: {
+        roboto: ["Roboto", "Helvetica", "Arial", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
