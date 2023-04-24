2c1dfac5ddaae483dc8f07f5ba3ad07ba60e2b4f
meghnaerla <meghna.e@iksula.com>
2023-04-24 17:37:21 +0530

    Modified shopping options

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ import Alpine from "alpinejs";
 
 window.Alpine = Alpine;
 Alpine.store("Menu", {
-  isVisible: false,
+  navIsVisible: false,
+  shoppingOptionsIsVisible: false,
 
-  toggle() {
+  toggleNav() {
     this.isVisible = !this.isVisible;
+  },
+  toggleShoppingOptions() {
+    this.shoppingOptionsIsVisible = !this.shoppingOptionsIsVisible;
   },
 });
 Alpine.start();
