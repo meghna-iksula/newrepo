7fb1a58a1bb348cacbbab88dae8d6d4251dc46d2
meghnaerla <meghna.e@iksula.com>
2023-04-25 15:40:01 +0530

    fixed errors

--- fullconfig.js
+++ fullconfig.js
@@ -589,7 +589,7 @@ export default {
       normal: "0em",
       wide: "0.025em",
       wider: "0.05em",
-      widest: "0.1em",
+      widest: "0.2em",
     },
     lineHeight: {
       none: "1",
